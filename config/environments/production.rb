0909bd35c10d4fccd19bb7be156e85b40a867425
Yusuke Miyazaki <ysk1180@gmail.com>
2019-01-27 20:32:42 +0900

    herokuデプロイエラー対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,8 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # エラー対応でコメントアウト（http://taremimi.hatenablog.jp/entry/2018/03/31/044914）
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
