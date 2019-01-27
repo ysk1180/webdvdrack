4d7d0b30baf9e99f81bc0877a3bb295b0700d56e
Yusuke Miyazaki <ysk1180@gmail.com>
2019-01-27 12:07:16 +0900

    [add]footer作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'posts#new'
   get 'search', to: 'posts#search'
   post 'make', to: 'posts#make'
+  get 'terms', to: 'terms#index', as: :terms
+  get 'privacy', to: 'privacies#index', as: :privacy
 end
